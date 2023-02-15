a4aac42b95d20d9c88ececf0856277e53d5176a7
Daniil <Daniil.@mail.ru>
2023-02-15 17:12:13 +0500

    code main

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 #В данном файле описывается основная работа алгоритма
 #Необходимо вызвать все файлы
 #
+import sys
 
+if(len(sys.argv) == 1):
+    print("Введите аргументы")
 
+if(len(sys.argv) == 4):
+    arv1 = float(sys.argv[1])
+    arv2 = float(sys.argv[3])
+
+    if(sys.argv[2] == "+"):
+        print(arv1 + arv2)
+    if(sys.argv[2] == "-"):
+        print(arv1 - arv2)
+    if(sys.argv[2] == "^"):
+        print(arv1 * arv2)
+    if(sys.argv[2] == "/"):
+        print(arv1 / arv2)        
